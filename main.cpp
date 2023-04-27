f851a6c629deca1706e97bfbc6e996cd8a90cf4d
Matt <matt@mattcarlotta.com>
2023-04-26 18:12:56 -0700

    chore: initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name = "Matt";
+    std::string favoriteFood = "streak";
+
+    std::cout << "Hello, my name is" << name << "and I like" << favoriteFood << std::endl;
+
+    return 0;
+}
